02c322e9e05a938591131dedf37f962a6f3bf57e
Ali Abrar <aliabrar@gmail.com>
2020-10-21 22:25:11 -0400

    ci: Fix readme symlinks in nix build

--- release.nix
+++ release.nix
@@ -1,1 +1,18 @@
-(import ./example {}).exe
+let ob = import ./example/.obelisk/impl {};
+    pkgs = ob.reflex-platform.nixpkgs;
+    srcNoSymlinks = pkgs.runCommand "deref-src" {} ''
+      mkdir $out
+      echo $out
+      mkdir -p $out/example/common/src
+      mkdir -p $out/example/reflex-gadt-api
+      cp -r ${./.}/* $out/example/reflex-gadt-api/
+      cat ${./Readme.md} > $out/example/common/src/Readme.lhs
+      ${pkgs.gnused}/bin/sed \
+        's~reflex-gadt-api = ./. + \"/../\"~reflex-gadt-api = ./reflex-gadt-api~g' \
+        ${./example/default.nix} > $out/example/default.nix
+      mkdir -p $out/example/.obelisk/impl
+      cp ${./example/.obelisk/impl/default.nix} $out/example/.obelisk/impl/default.nix
+      ${pkgs.rsync}/bin/rsync -arv --exclude='Readme.lhs' --exclude='default.nix' ${./example}/ $out/example/
+
+    '';
+in (import (srcNoSymlinks + "/example") {}).exe
