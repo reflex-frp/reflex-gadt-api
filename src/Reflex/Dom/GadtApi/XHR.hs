b117ad4f0664f75cc7b9e92fe170f185ca402080
Skylar-Ray O'Quinn <soraskymizu@gmail.com>
2022-08-23 03:52:04 +0000

    Use fetch in underlying HTTP requests

--- src/Reflex/Dom/GadtApi/XHR.hs
+++ src/Reflex/Dom/GadtApi/XHR.hs
@@ -4,8 +4,12 @@
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS -fno-warn-type-defaults #-}
+
 module Reflex.Dom.GadtApi.XHR where
 
+import Control.Lens hiding (has)
 import Control.Concurrent (forkIO, newEmptyMVar, putMVar, takeMVar)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Aeson
@@ -13,10 +17,24 @@ import qualified Data.ByteString.Lazy as LBS
 import Data.Constraint.Extras (Has, has)
 import Data.Functor (void)
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import Language.Javascript.JSaddle (MonadJSM)
+import Language.Javascript.JSaddle.Object ( fun )
+import Language.Javascript.JSaddle ( MonadJSM
+                                   , JSM
+                                   , obj
+                                   , js0
+                                   , js1
+                                   , jsgf
+                                   , (<#)
+                                   , toJSVal
+                                   , fromJSValUnchecked
+                                   , liftJSM
+                                   )
 import Language.Javascript.JSaddle.Monad (runJSM, askJSM)
 import Reflex.Dom.Core
+
+default (Text)
 
 type ApiEndpoint = Text
 
@@ -79,13 +97,44 @@ apiRequestXhr apiUrl = traverseRequesterData $ \x ->
       :: (MonadJSM m, FromJSON b)
       => api b
       -> m (Either Text b)
-    mkRequest req = do
-      response <- liftIO newEmptyMVar
-      _ <- newXMLHttpRequest (postJson apiUrl req) $
-        liftIO . putMVar response
-      xhrResp <- liftIO $ takeMVar response
-      case decodeXhrResponse xhrResp of
-        Nothing -> pure $ Left $
-          "Response could not be decoded for request: " <>
-            T.decodeUtf8 (LBS.toStrict $ encode req)
-        Just r -> pure $ Right r
+    mkRequest req =
+      liftJSM $ fetchPost apiUrl req
+
+-- | Make a POST reqeust via the fetch api
+fetchPost :: (ToJSON a, FromJSON b) => Text -> a -> JSM (Either Text b)
+fetchPost url a = do
+  headers <- obj
+  (headers <# "Content-Type") "application/json"
+
+  config <- obj
+  (config <# "method") "POST"
+  (config <# "cors") "no-cors"
+  (config <# "cache") "no-cache"
+  (config <# "body") (T.decodeUtf8 $ LBS.toStrict $ encode a)
+
+  mailbox <- liftIO newEmptyMVar
+  urlVal <- toJSVal url
+  configVal <- toJSVal config
+  promise <- jsgf "fetch" [urlVal, configVal]
+  let
+    textThenHandler = fun $ \_ _ [bodyVal] -> do
+      body <- fromJSValUnchecked bodyVal
+      let
+        ret = case eitherDecode $ LBS.fromStrict $ T.encodeUtf8 body of
+          Left err -> Left $ T.pack err
+          Right result -> Right result
+      liftIO $ putMVar mailbox ret
+
+    thenHandler = fun $ \_ _ [resp] -> do
+      bodyPromise <- resp ^. js0 "text"
+      _ <- bodyPromise ^. js1 "then" textThenHandler
+      pure ()
+
+    catchHandler = fun $ \_ _ [errVal] -> do
+      err :: Text <- fromJSValUnchecked errVal
+      liftIO $ putMVar mailbox $ Left err
+
+  _ <- promise ^. js1 "then" thenHandler
+  _ <- promise ^. js1 "catch" catchHandler
+
+  liftIO $ takeMVar mailbox
